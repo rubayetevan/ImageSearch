d162b9a95c61c43d881b21ae706c7d6969764de4
Md Rubayet Hassan <rubayet.evan@gmail.com>
2022-02-01 20:24:30 +0600

    HomeFragment.kt and DetailsFragment.kt added with layout resource file. Navigation graph added.

--- app/src/main/java/com/pluang/imagesearch/views/activities/MainActivity.kt
+++ app/src/main/java/com/pluang/imagesearch/views/activities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.pluang.imagesearch
+package com.pluang.imagesearch.views.activities
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.pluang.imagesearch.R
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/pluang/imagesearch/views/fragments/DetailsFragment.kt
@@ -0,0 +1,28 @@
+package com.pluang.imagesearch.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pluang.imagesearch.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private var _binding : FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
